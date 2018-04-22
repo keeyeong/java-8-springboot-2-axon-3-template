d4262ba81ebfdeab0882def049c9649e2b26c9b3
Kee-Yeong Tan <keeyeong.tan@gmail.com>
2018-04-22 19:20:35 +0200

    now it boots

---
+++ src/main/java/nl/tan/keeyeong/template/SampleApplication.java
@@ -0,0 +1,11 @@
+package nl.tan.keeyeong.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
